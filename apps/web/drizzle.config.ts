2909d2967225f527261209bd27c8fa25a7e68ab2
Mykhailo Andreitsiv <andreitsivdev@gmail.com>
2026-06-11 21:51:54 +0300

    fix: database url env

--- apps/web/drizzle.config.ts
+++ apps/web/drizzle.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'drizzle-kit';
 
+const isRailway = process.env.IS_RAILWAY;
+export const dataBaseUrl = isRailway
+  ? process.env.DATABASE_URL!
+  : process.env.DATABASE_PUBLIC_URL!;
+
 export default defineConfig({
   schema: './database/schema/*.ts',
   out: './database/migrations',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_PUBLIC_URL!,
+    url: dataBaseUrl,
   },
 });
